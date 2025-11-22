a8e8058cf935b0e36ac04f59b7d73b245bc2d9ae
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-11-22 05:54:00 +0530

    Create storage_channel.py

---
+++ plugins/storage_channel.py
@@ -0,0 +1,11 @@
+from config import Config, logger
+from typing import Any
+
+async def backup_file(client: Any, path: str, file_name: str, file_size: str, user: str, link: str):
+    logger.info(f"STORAGE: backup request {file_name} {file_size}")
+    if Config.STORAGE_CHANNEL:
+        try:
+            caption = f"📂 {file_name}\n🗃 {file_size}\n👤 {user}\n🔗 {link}"
+            await client.send_document(Config.STORAGE_CHANNEL, document=path, caption=caption)
+        except Exception as e:
+            logger.warning(f"Failed to backup to STORAGE_CHANNEL: {e}")
