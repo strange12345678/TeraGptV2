a9ffe95d6353560e252d2844e795d5d9e2a4c6be
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-11-22 06:38:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,9 @@ class Config:
     API_HASH = os.getenv("API_HASH", "e3096dde3e27c72a50e0e53d8ab23d6a")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "8593002387:AAHDSfKf7VURo5HemGum4Nza-LmvqDBD8lU")
 
-    # Optional channels (use -100... for groups)
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://testhubpro7:Q7iFC6EYg9URONBN@cluster0.0neghuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_DB = os.getenv("MONGO_DB", "terabox")
+
     LOG_CHANNEL = _env_int("LOG_CHANNEL", -1003347866431)
     ERROR_CHANNEL = _env_int("ERROR_CHANNEL", -1003347866431)
     STORAGE_CHANNEL = _env_int("STORAGE_CHANNEL", -1003347866431)
@@ -21,7 +23,7 @@ class Config:
     DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "downloads")
     IGNORE_MESSAGE_AGE = int(os.getenv("IGNORE_MESSAGE_AGE", 5))
 
-# setup logging to stdout for Koyeb
+# logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)-7s | %(name)s | %(message)s"
