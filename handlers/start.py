59acfdaded12bc3b96fc1c646fde84982b6427d9
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-11-22 07:53:41 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,25 @@
+# handlers/start.py
 from pyrogram import filters
-from script import Script
-from config import logger
+from pyrogram.types import Message
+import logging
+from config import Config
+from Theinertbotz.database import db
+
+log = logging.getLogger("TeraBoxBot")
+
+START_TEXT = """<b>👋 Welcome to TeraBox Demo Downloader</b>
+
+Send any TeraBox link and I'll fetch & download it for you.
+I process links one-by-one to avoid API errors.
+"""
 
 def register_handlers(app):
-    @app.on_message(filters.command("start") & filters.private)
-    async def start_cmd(_, msg):
-        logger.info(f"/start from {msg.from_user.id}")
-        await msg.reply(Script.START)
+    @app.on_message(filters.private & filters.command("start"))
+    async def start_cmd(client, message: Message):
+        try:
+            uid = message.from_user.id
+            db.add_user(uid)
+            await message.reply(START_TEXT, parse_mode="html")
+            log.info(f"/start from {uid}")
+        except Exception as e:
+            log.exception("start handler error")
