b17fc8a5d0f87194ab0e3305829d788ffc64ae8f
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-11-22 06:43:14 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
-from config import Config, logger
 from script import Script
+from config import logger
 
 def register_handlers(app):
     @app.on_message(filters.command("start") & filters.private)
-    async def start_cmd(client, msg):
+    async def start_cmd(_, msg):
         logger.info(f"/start from {msg.from_user.id}")
         await msg.reply(Script.START)
