72951355233e85b4e6d4bcbf72ca437e0bd9c8d0
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-11-22 05:58:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ def _env_int(name, default=0):
         return default
 
 class Config:
-    API_ID = int(os.getenv("API_ID", 0))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    API_ID = int(os.getenv("API_ID", 22582906))
+    API_HASH = os.getenv("API_HASH", "e3096dde3e27c72a50e0e53d8ab23d6a")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8593002387:AAHDSfKf7VURo5HemGum4Nza-LmvqDBD8lU")
 
     # Optional channels (use -100... for groups)
-    LOG_CHANNEL = _env_int("LOG_CHANNEL", 0)
-    ERROR_CHANNEL = _env_int("ERROR_CHANNEL", 0)
-    STORAGE_CHANNEL = _env_int("STORAGE_CHANNEL", 0)
+    LOG_CHANNEL = _env_int("LOG_CHANNEL", -1003347866431)
+    ERROR_CHANNEL = _env_int("ERROR_CHANNEL", -1003347866431)
+    STORAGE_CHANNEL = _env_int("STORAGE_CHANNEL", -1003347866431)
 
     DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "downloads")
     IGNORE_MESSAGE_AGE = int(os.getenv("IGNORE_MESSAGE_AGE", 5))
